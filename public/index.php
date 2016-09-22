496166ecbd848c82555c6ea4b7c7a2f047c0549b
Terry Gilliver <terry@mrtaxsoftware.com>
2016-09-22 13:23:06 +0100

    Removed extra retstserver and ignored pdf and jpg dirs

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 require __DIR__ . '/../database/initialize.php';
-require __DIR__ . '/../Jacwright/RestServer/RestServer.php';
+require __DIR__ . '/../database/Jacwright/RestServer/RestServer.php';
 require __DIR__ . '/class.OWDDBController.php';
 //redirect_to('test.php');
 $server = new \Jacwright\RestServer\RestServer('debug');
